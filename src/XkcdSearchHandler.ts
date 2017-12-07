6c6195913c7936906d5b7f40700d53045a5a9e01
Y0hy0h <Y0hy0h@users.noreply.github.com>
2017-12-07 14:35:07 +0100

    Move message generation to `MessageProvider`

--- src/XkcdSearchHandler.ts
+++ src/XkcdSearchHandler.ts
@@ -1,14 +1,17 @@
-import * as loadJsonFile from 'load-json-file'
 import * as XKCD from 'relevant-xkcd'
 import * as Telegraf from 'telegraf'
-import * as Extra from 'telegraf/extra'
-import * as Markup from 'telegraf/markup'
 import { ContextMessageUpdate } from 'telegraf/typings'
 import { Message } from 'telegraf/typings/telegram-types'
-import { InlineQueryResultArticle, InputMessageContent } from './telegram'
+import { MessageProvider } from './messageProvider'
+import { InlineQueryResult } from './telegram'
 
 export class XkcdSearchHandler {
     private botName: string
+    private messageProvider: MessageProvider
+
+    private constructor() {
+        this.messageProvider = new MessageProvider()
+    }
 
     static async createWith(bot: Telegraf) {
         const handler = new XkcdSearchHandler()
@@ -26,30 +29,8 @@ export class XkcdSearchHandler {
     }
 
     async help(context: ContextMessageUpdate): Promise<Message> {
-        const replyText = `\
-Type \`@${this.botName} <query>\` in any chat to search for [relevant xkcd](https://relevantxkcd.appspot.com/).
-
-You can also use \`/xkcd <query>\` in pm or any group I'm in.
-
-When query is empty, latest xkcd is sent.`
-
-        const npmPackage = await loadJsonFile('package.json')
-        const contactUrl = process.env.CONTACT_URL || 't.me/GingerPlusPlus'
-        const inlineKeyboard = Markup.inlineKeyboard(
-            [
-                [
-                    Markup.urlButton('Contact author', contactUrl),
-                ],
-                [
-                    Markup.urlButton('Repository', npmPackage.repository.url),
-                ],
-                [
-                    Markup.urlButton('Rate', 'https://telegram.me/storebot?start=xkcdsearch_bot'),
-                ],
-            ],
-        )
-
-        return context.reply(replyText, Extra.markdown().markup(inlineKeyboard))
+        const message = this.messageProvider.getHelpMessage(this.botName)
+        return message.asReply(context)
     }
 
     async inlineQuery(context: ContextMessageUpdate) {
@@ -57,31 +38,10 @@ When query is empty, latest xkcd is sent.`
         const relevantComics: Array<XKCD.Comic> = await XKCD.fetchAllRelevant(query)
         const offset = parseInt(context.inlineQuery.offset) || 0
         const splicedRelevantComics = relevantComics.splice(offset)
-        const results = await Promise.all(splicedRelevantComics.map(async (comic: XKCD.Comic) => {
-            return new InlineQueryResultArticle(
-                comic.id.toString(),
-                comic.title,
-                new InputMessageContent(
-                    comic.xkcdURL,
-                ),
-                Markup.inlineKeyboard([
-                                          [
-                                              Markup.callbackButton('Show mouse-over', comic.id),
-                                          ],
-                                          [
-                                              Markup.urlButton(
-                                                  'Explain',
-                                                  comic.explainURL,
-                                              ),
-                                          ],
-                                      ],
-                ),
-                comic.xkcdURL,
-                comic.transcript,
-                comic.imageURL,
-            )
+        const results: Array<InlineQueryResult> = await Promise.all(splicedRelevantComics.map(async (comic: XKCD.Comic) => {
+            return this.messageProvider.makeInlineQueryResultFromComic(comic)
         }))
-        context.answerInlineQuery(
+        return context.answerInlineQuery(
             results as any,
             {next_offset: offset + results.length, cache_time: 0} as any,
         )
@@ -90,7 +50,8 @@ When query is empty, latest xkcd is sent.`
     async mouseOver(context: ContextMessageUpdate) {
         const xkcdId = parseInt(context.callbackQuery.data)
         const comic = await XKCD.fetchComic(xkcdId)
-        context.answerCallbackQuery(comic.altText, undefined, true)
+        const answer = this.messageProvider.getCallbackQueryAnswerForComic(comic)
+        return answer.asAnswer(context, 0)
     }
 }
 
---
+++ src/messageProvider.ts
@@ -0,0 +1,71 @@
+import * as loadJsonFile from 'load-json-file'
+import * as XKCD from 'relevant-xkcd'
+import * as Markup from 'telegraf/markup'
+import {
+    CallbackQueryAnswer, InlineQueryResult, InlineQueryResultArticle, InputMessageContent, Message,
+    MessageText,
+} from './telegram'
+
+export class MessageProvider {
+    getHelpMessage(botName: string) {
+        const message_text = new MessageText(
+            `\
+Type \`@${botName} <query>\` in any chat to search for [relevant xkcd](https://relevantxkcd.appspot.com/).
+
+You can also use \`/xkcd <query>\` in pm or any group I'm in.
+
+When query is empty, latest xkcd is sent.`,
+            MessageText.Markdown,
+        )
+
+        const contactUrl = process.env.CONTACT_URL || 't.me/GingerPlusPlus'
+        const npmPackage = loadJsonFile.sync('package.json')
+        const repositoryUrl = npmPackage.repository.url
+        const rateUrl = `https://telegram.me/storebot?start=${botName}`
+        const inlineKeyboard = Markup.inlineKeyboard(
+            [
+                [
+                    Markup.urlButton('Contact author', contactUrl),
+                ],
+                [
+                    Markup.urlButton('Repository', repositoryUrl),
+                ],
+                [
+                    Markup.urlButton('Rate', rateUrl),
+                ],
+            ],
+        )
+
+        return new Message(message_text, inlineKeyboard)
+    }
+
+    makeInlineQueryResultFromComic(comic: XKCD.Comic): InlineQueryResult {
+        return new InlineQueryResultArticle(
+            comic.id.toString(),
+            comic.title,
+            new InputMessageContent(
+                comic.xkcdURL,
+            ),
+            Markup.inlineKeyboard(
+                [
+                    [
+                        Markup.callbackButton('Show mouse-over', comic.id),
+                    ],
+                    [
+                        Markup.urlButton(
+                            'Explain',
+                            comic.explainURL,
+                        ),
+                    ],
+                ],
+            ),
+            comic.xkcdURL,
+            comic.transcript,
+            comic.imageURL,
+        )
+    }
+
+    getCallbackQueryAnswerForComic(comic: XKCD.Comic): CallbackQueryAnswer {
+        return new CallbackQueryAnswer(comic.altText, true)
+    }
+}

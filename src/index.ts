4c919304ac70b8936ca2206609da87393ed7acc2
Y0hy0h <Y0hy0h@users.noreply.github.com>
2017-12-11 12:05:02 +0100

    Add and apply standard code style

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
 import * as Express from 'express'
 import * as Proxy from 'express-http-proxy'
+import * as path from 'path'
 import { startBot } from './startBot'
 
 startServer(Express())
 
-async function startServer(app: Express) {
-    // Static website
-    app.use(Express.static(__dirname + '/public/'))
+async function startServer (app: Express) {
+  // Static website
+  app.use(Express.static(path.join(__dirname, '/public/')))
 
-    // Bot webhooks redirect
-    const {pathToFetchFrom, urlToRedirectTo} = await startBot()
-    app.use(pathToFetchFrom, Proxy(urlToRedirectTo))
+  // Bot webhooks redirect
+  const {pathToFetchFrom, urlToRedirectTo} = await startBot()
+  app.use(pathToFetchFrom, Proxy(urlToRedirectTo))
 
-    const listener = app.listen(process.env.PORT, function () {
-        console.log('Your app is listening on port ' + listener.address().port)
-    })
+  const listener = app.listen(process.env.PORT, function () {
+    console.log('Your app is listening on port ' + listener.address().port)
+  })
 }
--- src/startBot.ts
+++ src/startBot.ts
@@ -2,33 +2,32 @@ import * as Telegraf from 'telegraf'
 import * as util from 'util'
 import { XkcdSearchHandler } from './bot/xkcdSearchHandler'
 
-export async function startBot() {
-    const token = process.env.BOT_TOKEN
-    const bot = new Telegraf(token)
-    bot.use(log_middleware)
-    bot.catch(err => console.error(err))
+export async function startBot () {
+  const token = process.env.BOT_TOKEN
+  const bot = new Telegraf(token)
+  bot.use(logMiddleware)
+  bot.catch(err => console.error(err))
 
-    await XkcdSearchHandler.createWith(bot)
+  await XkcdSearchHandler.createWith(bot)
 
-    const domain = process.env.PROJECT_DOMAIN
-    const host = `${domain}.glitch.me`
-    const port = 8443
-    const pathToFetchFrom = `/bot/${token}`
-    await bot.telegram.setWebhook(`https://${host}${pathToFetchFrom}`)
-    bot.startWebhook(`/`, null, port, 'localhost')
-    console.log(`Your bot is listening on port ${port}.`)
+  const domain = process.env.PROJECT_DOMAIN
+  const host = `${domain}.glitch.me`
+  const port = 8443
+  const pathToFetchFrom = `/bot/${token}`
+  await bot.telegram.setWebhook(`https://${host}${pathToFetchFrom}`)
+  bot.startWebhook(`/`, null, port, 'localhost')
+  console.log(`Your bot is listening on port ${port}.`)
 
-    return {
-        pathToFetchFrom: pathToFetchFrom,
-        urlToRedirectTo: `localhost:${port}`,
-    }
+  return {
+    pathToFetchFrom: pathToFetchFrom,
+    urlToRedirectTo: `localhost:${port}`
+  }
 }
 
-async function log_middleware(ctx, next) {
-    console.log('===================================================')
-    console.log('Incoming:\n', util.inspect(ctx.update, {depth: 5}))
-    console.log('- - - - - - - - - - - - - - - - - - - - - - - - - -')
-    console.log('Outgoing:\n', await next())
-    console.log('===================================================')
+async function logMiddleware (ctx, next) {
+  console.log('===================================================')
+  console.log('Incoming:\n', util.inspect(ctx.update, {depth: 5}))
+  console.log('- - - - - - - - - - - - - - - - - - - - - - - - - -')
+  console.log('Outgoing:\n', await next())
+  console.log('===================================================')
 }
-
